e9644765c22d4dc02908102243e279cd1daaa9b0
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-06 09:44:55 -0600

    Add functionality to allow creation of new file from command line; delete README (will add back later)

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,10 @@
 message, braille = ARGV
-@message_read = File.read("./#{message}")
-@message_open = File.open("./#{message}")
-@braille_file = braille
-puts "created #{braille} containing #{@message_read.length - 1} characters"
+message_open = File.open("./#{message}").read
+# message = message_open.read
+# braille_file = braille
+braille_file = File.open("./#{braille}", "w")
+puts "created #{braille} containing #{message_open.length - 1} characters"
+
+# braille_file.puts "I wrote this!"
+braille_file.puts message_open
+# braille_file.close
