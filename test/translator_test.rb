2fe3b730a02fe9a55ab6900b9dd6099715181d4c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-06 22:46:57 -0600

    Try to refactor to translate multiple letters with no success

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -8,30 +8,35 @@ class TranslatorTest < Minitest::Test
     @message = File.open(ARGV[0], "r")
     @read_message = @message.read.chomp
     @braille = File.open(ARGV[1], "w")
-    # @write_braille = @braille.write(@read_message)
-    # @translator = Translator.new(@read_message, @write_braille)
     @translator = Translator.new(@read_message, @braille)
   end
 
   def test_it_exists
+    skip
     assert_instance_of Translator, @translator
   end
 
   def test_it_has_attributes
+    skip
     #Use mocks and stubs here
     assert_instance_of Hash, @translator.braille_hash
     assert_equal "a", @translator.message
-    # assert_equal 4, @translator.braille
     assert_instance_of File, @translator.braille
     assert_equal 1, @translator.write_braille(@read_message)
   end
 
   def test_it_can_turn_message_into_array
+    skip
     assert_equal ["a"], @translator.message_to_array
   end
 
   def test_it_can_translate_a_letter
+    skip
     assert_equal "0.\n..\n..", @translator.translate
     assert_equal 8, @translator.write_braille(@translator.translate)
+  end
+
+  def test_it_can_translate_two_letters
+    assert_equal "0.0.\n..0.\n....", @translator.translate
   end
 end
