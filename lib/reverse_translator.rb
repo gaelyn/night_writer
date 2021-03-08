5b0841623b59257e799682b30d5fc31c5b55996e
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-07 20:33:40 -0600

    Add translate method that will translate one character

--- lib/reverse_translator.rb
+++ lib/reverse_translator.rb
@@ -11,4 +11,23 @@ class ReverseTranslator
   def welcome
     "Created #{ARGV[1]} containing #{@braille.length} characters"
   end
+
+  def braille_to_array
+    @braille.split
+  end
+
+  def write_english(message)
+    @original.write(message)
+    @original.close
+  end
+
+  def read_original
+    File.open(ARGV[1], "r").read
+  end
+
+  def translate
+    braille_to_array.map do |char|
+      dictionary.english_hash[char]
+    end.join
+  end
 end
--- test/reverse_translator_test.rb
+++ test/reverse_translator_test.rb
@@ -9,7 +9,7 @@ class ReverseTranslatorTest < Minitest::Test
     @braille = File.open(ARGV[0], "r")
     @read_braille = @braille.read.chomp
     @original_message = File.open(ARGV[1], "w+")
-    @read_message = File.open(ARGV[1], "r")
+    # @read_original = File.open(ARGV[1], "r")
     @reverse = ReverseTranslator.new(@read_braille, @original_message)
   end
 
@@ -28,5 +28,17 @@ class ReverseTranslatorTest < Minitest::Test
   def test_it_can_print_welcome_message
     skip
     assert_equal "Created original.txt containing 8 characters", @reverse.welcome
+  end
+
+  def test_it_can_turn_braille_into_array
+    skip
+    assert_equal ["0.", "..", ".."], @reverse.braille_to_array
+  end
+
+  def test_it_can_write_to_new_file
+    # skip
+    @reverse.write_english("a")
+
+    assert_equal "a", @reverse.read_original
   end
 end
