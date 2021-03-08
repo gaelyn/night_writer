bbc6bf3118771ec5c5c1232066ec5a6d68877df8
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-07 19:18:14 -0600

    Add welcome message to runner

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,10 @@
 require './lib/translator'
+require './lib/dictionary'
 
   message = File.open(ARGV[0], "r")
   read_message = message.read.chomp
   braille = File.open(ARGV[1], "w")
   translator = Translator.new(read_message, braille)
+
+  puts translator.welcome
   translator.write_braille(translator.translate)
