9a33faebfcd66f77feccb37040a5315ff5c746bb
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-09 08:39:29 -0600

    Add one mock to reverse translator test

--- test/reverse_translator_test.rb
+++ test/reverse_translator_test.rb
@@ -13,41 +13,36 @@ class ReverseTranslatorTest < Minitest::Test
   end
 
   def test_it_exists
-    # skip
     assert_instance_of ReverseTranslator, @reverse
   end
 
   def test_it_has_attributes
-    # skip
     assert_equal "0.\n..\n..", @reverse.braille
     assert_instance_of File, @reverse.original
     assert_instance_of Dictionary, @reverse.dictionary
   end
 
   def test_it_can_print_welcome_message
-    # skip
     assert_equal "Created original.txt containing 1 characters", @reverse.welcome
   end
 
   def test_it_can_turn_braille_into_array
-    # skip
     assert_equal ["0.", "..", ".."], @reverse.braille_to_array
   end
 
   def test_it_can_write_to_new_file
-    # skip
     @reverse.write_english("a")
     assert_equal "a", @reverse.read_original
   end
 
   def test_it_can_split_lines_back_to_braille_arrays
-    skip
-    #used "ab" to test this instead of "a"
-    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], @reverse.split_out_letters
+    new_braille = "0.0.\n..0.\n...."
+    reverse = ReverseTranslator.new(new_braille, @original_message)
+
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], reverse.split_out_letters
   end
 
   def test_it_can_translate_to_english
-    # skip
     assert_equal "a", @reverse.translate
   end
 end
