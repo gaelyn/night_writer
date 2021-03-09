ac68ab77d4c42a39da590d93e5bd00d91c1992ca
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-09 08:43:29 -0600

    Format runner files

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,10 +1,10 @@
 require './lib/reverse_translator'
 require './lib/dictionary'
 
-  braille = File.open(ARGV[0], "r")
-  read_braille = braille.read.chomp
-  message = File.open(ARGV[1], "w")
-  reverse = ReverseTranslator.new(read_braille, message)
+braille = File.open(ARGV[0], "r")
+read_braille = braille.read.chomp
+message = File.open(ARGV[1], "w")
+reverse = ReverseTranslator.new(read_braille, message)
 
-  puts reverse.welcome
-  reverse.write_english(reverse.translate)
+puts reverse.welcome
+reverse.write_english(reverse.translate)
